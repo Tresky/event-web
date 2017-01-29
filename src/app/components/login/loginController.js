1a45cc0f1edc7d8cdbc55ef99915842aa2a74297
TylerGauntlett <tylergauntlett@hotmail.com>
2017-01-28 19:19:26 -0500

    Added login page and removed unused index style sheet.

---
+++ src/app/components/login/loginController.js
@@ -0,0 +1,11 @@
+import './loginStyles.styl'
+
+angular.module('app')
+  .controller('LoginController', function ($log) {
+    var vm = this
+    vm.test = 'testing'
+    vm.func = () => {
+      $log.log('Function was called')
+    }
+  }
+)
