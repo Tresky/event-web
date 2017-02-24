dc22c28fb0043f0ba9b0fdc74a9635c139970cb1
Tyler <tnpetresky@gmail.com>
2017-02-24 14:51:41 -0500

    Relative URLs (#7)
    * Merge conflict
    
    * Changes to eslint config
    
    * Added the ServerUrls service and updated the http services to use relative paths
    
    * Changed $routeProvider#otherwise condition to home
    
    * Removed left over debugging statement

--- src/app/index.js
+++ src/app/index.js
@@ -2,7 +2,7 @@ import angular from 'angular'
 import angularCookies from 'angular-cookie'
 import satellizer from 'satellizer'
 import uiRouter from 'angular-ui-router'
-import _ from 'lodash'
+import * as _ from 'lodash'
 
 import './styles/index.styl'
 
@@ -14,78 +14,83 @@ angular.module('app', [
   satellizer,
   uiRouter
   // 'vModal'
-]).config([
-  '$stateProvider',
-  '$urlRouterProvider',
-  '$locationProvider',
-  '$authProvider',
-  ($stateProvider, $urlRouterProvider, $locationProvider, $authProvider) => {
-    // Allow the use of HTML5 state modes
-    $locationProvider.html5Mode(true)
+]).constant('_', _)
+  .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    '$locationProvider',
+    '$authProvider',
+    '$windowProvider',
+    ($stateProvider, $urlRouterProvider, $locationProvider, $authProvider, $windowProvider) => {
+      // Allow the use of HTML5 state modes
+      $locationProvider.html5Mode(true)
 
-    let loginRequired = ['$q', '$state', '$auth', ($q, $state, $auth) => {
-      let deferred = $q.defer()
-      if ($auth.isAuthenticated()) {
-        deferred.resolve()
-      } else {
-        $state.go('login')
-      }
-      return deferred.promise
-    }]
-
-    // Define all of the application states
-    $stateProvider
-      .state('home', {
-        url: '/',
-        template: require('./components/home/homeView.html'),
-        controller: 'HomeController',
-        controllerAs: 'homeCtrl',
-        onEnter: ['$state', '$auth', ($state, $auth) => {
-          if ($auth.isAuthenticated()) {
-            $state.go('dashboard')
-          }
-        }]
-      })
-      .state('dashboard', {
-        url: '/dashboard',
-        template: require('./components/dashboard/dashboardView.html'),
-        controller: 'DashboardController',
-        controllerAs: 'dashCtrl',
-        resolve: {
-          loginRequired: loginRequired
+      let loginRequired = ['$q', '$location', '$auth', ($q, $location, $auth) => {
+        let deferred = $q.defer()
+        if ($auth.isAuthenticated()) {
+          deferred.resolve()
+        } else {
+          $location.path('/login')
         }
-      })
-      .state('login', {
-        url: '/login',
-        template: require('./components/login/loginView.html'),
-        controller: 'LoginController',
-        controllerAs: 'loginCtrl',
-        onEnter: ['$state', '$auth', ($state, $auth) => {
-          if ($auth.isAuthenticated()) {
-            $state.go('dashboard')
+        return deferred.promise
+      }]
+
+      // Define all of the application states
+      $stateProvider
+        .state('home', {
+          url: '/',
+          template: require('./components/home/homeView.html'),
+          controller: 'HomeController',
+          controllerAs: 'homeCtrl',
+          onEnter: ['$state', '$auth', ($state, $auth) => {
+            if ($auth.isAuthenticated()) {
+              $state.go('dashboard')
+            }
+          }]
+        })
+        .state('dashboard', {
+          url: '/dashboard',
+          template: require('./components/dashboard/dashboardView.html'),
+          controller: 'DashboardController',
+          controllerAs: 'dashCtrl',
+          resolve: {
+            loginRequired: loginRequired
           }
-        }]
-      })
-      .state('logout', {
-        url: '/logout',
-        onEnter: ['$state', '$auth', ($state, $auth) => {
-          $auth.logout()
-          $state.go('login')
-        }]
-      })
-      .state('event', {
-        url: '/event',
-        template: require('./components/event/eventView.html'),
-        controller: 'EventController',
-        controllerAs: 'vm'
-      })
+        })
+        .state('login', {
+          url: '/login',
+          template: require('./components/login/loginView.html'),
+          controller: 'LoginController',
+          controllerAs: 'loginCtrl',
+          onEnter: ['$state', '$auth', ($state, $auth) => {
+            if ($auth.isAuthenticated()) {
+              $state.go('dashboard')
+            }
+          }]
+        })
+        .state('logout', {
+          url: '/logout',
+          onEnter: ['$state', '$auth', ($state, $auth) => {
+            $auth.logout()
+            $state.go('login')
+          }]
+        })
 
-    // If no states were matched, redirect to home
-    $urlRouterProvider.otherwise('home')
+      // If no states were matched, redirect to home
+      $urlRouterProvider.otherwise('home')
 
-    $authProvider.baseUrl = 'http://localhost:3000/api'
-    $authProvider.loginUrl = '/auth/login'
-  }])
+      let $window = $windowProvider.$get()
+
+      // There isn't a better way to set this value more dynamically
+      // due to contraints provided by the Satellizer package and Angular.
+      let localUrls = ['localhost', '127.0.0.1', '192.168']
+      if (_.includes(localUrls, $window.location.hostname)) {
+        $authProvider.baseUrl = 'http://localhost:3000/api'
+      } else {
+        $authProvider.baseUrl = '/'
+      }
+      $authProvider.loginUrl = '/auth/login'
+    }])
 
 require('./services/index.js')
 require('./components/index.js')
--- src/app/services/index.js
+++ src/app/services/index.js
@@ -1,5 +1,8 @@
+// Services
+import './ServerUrls.js'
+
+// HTTP Services
 import './http-services/University.js'
 import './http-services/User.js'
 import './http-services/Rso.js'
 import './http-services/Membership.js'
-
