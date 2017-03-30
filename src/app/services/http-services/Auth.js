447b820e7be8348ad168c8f3d60cd7df255be8cc
maximus64 <admin@khoahoang.com>
2017-03-30 17:29:50 -0400

    Hook register to backend + Fix some bugs in http-services

---
+++ src/app/services/http-services/Auth.js
@@ -0,0 +1,27 @@
+angular.module('app')
+  .factory('Auth', ($q, $http, ServerUrls, _) => {
+    let baseUrl = ServerUrls.current.nodeApi + 'auth'
+
+    let Auth = (initData) => {
+      let auth = {}
+      angular.extend(auth, initData)
+
+      // Instance Methods
+      return auth
+    }
+
+    // Class Methods
+    let mAuth = {
+      // Creates a new Rso
+      signup (data) {
+        return $http.post(baseUrl + '/signup', data)
+          .then((res) => {
+            return new Auth(res.data)
+          }, (response) => {
+            return $q.reject(response)
+          })
+      },
+    }
+
+    return mAuth
+  })
--- src/app/services/http-services/Rso.js
+++ src/app/services/http-services/Rso.js
@@ -16,7 +16,7 @@ angular.module('app')
       findAll (params) {
         return $http.get(baseUrl, params)
           .then((rso) => {
-            return _.map(rso, (i) => {
+            return _.map(rso.data, (i) => {
               return new Rso(i)
             })
           }, (response) => {
@@ -27,7 +27,7 @@ angular.module('app')
       findById (id) {
         return $http.get(baseUrl + '/' + id)
           .then((rso) => {
-            return Rso.new(rso)
+            return new Rso(rso.data)
           }, (response) => {
             return $q.reject(response)
           })
@@ -37,7 +37,7 @@ angular.module('app')
       create (data) {
         return $http.post(baseUrl, data)
           .then((rso) => {
-            return Rso.new(rso)
+            return new Rso(rso.data)
           }, (response) => {
             return $q.reject(response)
           })
@@ -46,7 +46,7 @@ angular.module('app')
       update (params) {
         return $http.put(baseUrl + '/' + params.id, params)
           .then((rso) => {
-            return Rso.new(rso)
+            return new Rso(rso.data)
           }, (response) => {
             return $q.reject(response)
           })
@@ -55,7 +55,7 @@ angular.module('app')
       destroy (id) {
         return $http.delete(baseUrl + '/' + id)
           .then((rso) => {
-            return Rso.new(rso)
+            return new Rso(rso.data)
           }, (response) => {
             return $q.reject(response)
           })
