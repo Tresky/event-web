f40e20fb53da0dbcd55cf6067af015991ef85df0
maximus64 <admin@khoahoang.com>
2017-04-11 18:06:33 -0400

    RSO controller

--- src/app/components/rso/rsoController.js
+++ src/app/components/rso/rsoController.js
@@ -9,10 +9,26 @@
 import './rsoStyles.styl'
 
 angular.module('app')
-  .controller('RsoController', function ($log) {
+  .controller('RsoController', function ($log, $location, Rso) {
     var vm = this
     vm.test = 'testing'
-    vm.func = () => {
+
+    var rsoId = $location.search().rid
+    var uniId = $location.search().uid
+
+    //TODO: enable this when backend is ready (getting 403 error atm)
+    // Rso.findById(uniId,rsoId)
+    //   .then((response) => {
+    //     vm.rsoData = response
+    //     $log.log('Success', response)
+    //   }, (response) => {
+    //     $log.log('Failure', response)
+    //   })
+
+    vm.rsoData = {name: "meh", description: "What do you want?", university_name: "Trump University", privacy_level: "None"}
+    vm.eventData  = [{name: "event 1"}, {name: "event 2"}, {name: "event 3"}, {name: "event 4"}]
+
+    vm.subscribe = () => {
       $log.log('Function was called')
     }
   }
