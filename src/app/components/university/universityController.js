58ced68e4b86d5dfe4aa135cfd8ce813e1e67d69
maximus64 <admin@khoahoang.com>
2017-04-11 18:21:44 -0400

    add univeristy controller logic

--- src/app/components/university/universityController.js
+++ src/app/components/university/universityController.js
@@ -9,11 +9,25 @@
 import './universityStyles.styl'
 
 angular.module('app')
-  .controller('UniversityController', function ($log) {
+  .controller('UniversityController', function ($log, $location, University) {
     var vm = this
     vm.test = 'testing'
     vm.func = () => {
       $log.log('Function was called')
     }
+
+    var uniId = $location.search().uid
+
+    University.findById(uniId)
+      .then((response) => {
+        vm.uniData = response
+        $log.log('Success', response)
+      }, (response) => {
+        $log.log('Failure', response)
+      })
+
+    vm.rsoData  = [{name: "RSOOOOO 1"}, {name: "RSOOOOO 2"}, {name: "RSOOOOO 3"}, {name: "RSOOOOO 4"}]
+    //vm.uniData = {name: "Trump University", description: "Very good university", num_of_students: "0"}
+
   }
 )
